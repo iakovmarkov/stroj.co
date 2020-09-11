f247b11562346e8f26982c48082b551e14a96959
iakovmarkov <iakovmarkov@users.noreply.github.com>
2020-09-11 12:59:18 +0000

    deploy: 582b88321c19f5be944065ef8cc4797e5e4f37fd

---
+++ precache-manifest.d988c91fd03fb5f8ae8b7bb2a2150a41.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7da9812730ea061d027cae36007aac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4253925abbff757967e9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "00ea791f97fc84362b0b",
+    "url": "/static/js/2.9f6d8871.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9f6d8871.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4253925abbff757967e9",
+    "url": "/static/js/main.aca853f5.chunk.js"
+  },
+  {
+    "revision": "b16bb79d2f9fa18250f8",
+    "url": "/static/js/runtime-main.e4c61410.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
